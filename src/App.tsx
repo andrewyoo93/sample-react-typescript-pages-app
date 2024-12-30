1fd0f314bb82f0117ec4f49dce71e53eddec0c98
andrewyoo93 <andrew.yoo@gethealthie.com>
2024-12-30 13:13:06 -0500

    Testing javascript observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useState } from "react";
+import HelpScout, { NOTIFICATION_TYPES } from "@helpscout/javascript-sdk";
+import { useEffect, useState } from "react";
 
 function App() {
   const loc = window.location.toString();
   const params = loc.split("/");
-  const [resultData, setResultData] = useState<any>()
+  const [resultData, setResultData] = useState<any>(undefined);
 
   const onClick = async () => {
     // testing
@@ -15,14 +16,46 @@ function App() {
     console.log(result.data);
   };
 
+  function onTemplateClick() {
+    HelpScout.showNotification(
+      NOTIFICATION_TYPES.SUCCESS,
+      "Hello from the sidebar app"
+    );
+  }
+
+  const appContainer = document.querySelector('#app-container');
+
+  useEffect(() => {
+    if (appContainer != null && resultData != null) {
+      const resizeObserver = new ResizeObserver(() => {
+        HelpScout.setAppHeight(appContainer.clientHeight);
+      });
+
+      resizeObserver.observe(appContainer);
+
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, [appContainer, resultData]);
+
   return (
     <div className="App">
-      <div onClick={onClick}>
-        Click me
+      <h1 >Hi, test</h1>
+      <p>The conversation is test</p>
+      <br />
+      <div onClick={onTemplateClick}>
+        Click me for banner
       </div>
-      {resultData != null &&
-        resultData?.message
-      }
+
+      <div className="App">
+        <div onClick={onClick}>
+          Click me for axios
+        </div>
+        {resultData != null &&
+          resultData?.message
+        }
+      </div>
     </div>
   );
 }
